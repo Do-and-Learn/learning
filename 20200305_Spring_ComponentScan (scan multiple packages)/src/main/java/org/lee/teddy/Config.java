895ed2af2b452dc52fe8277bcf584bd2c2e8aebc
Teddy Lee <teddy12090@gmail.com>
2020-03-05 22:44:00 +0800

    2020/03/05 Scan multiple packages

---
+++ 20200305_Spring_ComponentScan (scan multiple packages)/src/main/java/org/lee/teddy/Config.java
@@ -0,0 +1,9 @@
+package org.lee.teddy;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages={"org.lee.teddy", "org.universal"})
+public class Config {
+}
