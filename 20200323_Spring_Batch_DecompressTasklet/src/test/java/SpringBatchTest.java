04a2eb30542c329a1885832e8bd5b78fa2f3b6c1
Teddy Lee <teddy12090@gmail.com>
2020-03-23 23:17:09 +0800

    2020/03/23 Spring Batch Decompress

---
+++ 20200323_Spring_Batch_DecompressTasklet/src/test/java/SpringBatchTest.java
@@ -0,0 +1,51 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@ContextConfiguration(locations = {"classpath:beans.xml", "classpath:config.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringBatchTest {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeTestMethod
+    public void setup() {
+        jdbcTemplate.update("delete from USER");
+    }
+
+    @Test
+    public void testJob() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+        Assert.assertEquals(0, jdbcTemplate.queryForInt("select count(1) from USER"));
+        jobLauncher.run(job, new JobParametersBuilder()
+                .addString("inputResource", "simple.zip")
+                .addString("targetFile", "decompress.csv")
+                .addLong("timestamp", System.currentTimeMillis()).toJobParameters());
+        Assert.assertEquals(2, jdbcTemplate.queryForInt("select count(1) from USER"));
+        jobLauncher.run(job, new JobParametersBuilder()
+                .addString("inputResource", "simple.zip")
+                .addString("targetFile", "decompress.csv")
+                .addLong("timestamp", System.currentTimeMillis()).toJobParameters());
+        Assert.assertEquals(4, jdbcTemplate.queryForInt("select count(1) from USER"));
+    }
+}
