7897b09c6cf6dfdc7988f76156be8a53a4cb5f9f
Teddy Lee <teddy12090@gmail.com>
2020-05-02 00:37:41 +0800

    20200501 search vs match

---
+++ 2020/05/20200501_search_vs_match/search_vs_match_test.py
@@ -0,0 +1,38 @@
+import re
+import unittest
+
+
+class SearchVsMatchTest(unittest.TestCase):
+
+    def test_1(self):
+        with self.subTest("match"):
+            match = re.match("c", "abcdef")
+            self.assertIsNone(match)
+
+        with self.subTest("search"):
+            search = re.search("c", "abcdef")
+            self.assertIsNotNone(search)
+            self.assertEqual((2, 3), search.span())
+
+    def test_2(self):
+        search = re.search("^c", "abcdef")
+        self.assertIsNone(search)
+
+    def test_3(self):
+        search = re.search("^a", "abcdef")
+        self.assertIsNotNone(search)
+        self.assertEqual((0, 1), search.span())
+
+    def test_4(self):
+        with self.subTest("match"):
+            match = re.match('X', 'A\nB\nX', re.MULTILINE)
+            self.assertIsNone(match)
+
+        with self.subTest("search"):
+            search = re.search('^X', 'A\nB\nX', re.MULTILINE)
+            self.assertIsNotNone(search)
+            self.assertEqual((4, 5), search.span())
+
+
+if __name__ == '__main__':
+    unittest.main()
