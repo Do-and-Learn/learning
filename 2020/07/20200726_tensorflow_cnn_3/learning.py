3853c3d084b8c4e776cb24ed21892ba5f8ad40dc
Teddy Lee <teddy12090@gmail.com>
2020-07-26 20:09:30 +0800

    Epoch 20/20 782/782 [==============================] - 443s 566ms/step - loss: 0.6896 - accuracy: 0.7618 - val_loss: 0.5910 - val_accuracy: 0.8046 79/79 [==============================] - 17s 220ms/step - loss: 0.5910 - accuracy: 0.8046 Test score 0.5910143256187439 Test accuracy 0.8046000003814697

--- 2020/07/20200726_tensorflow_cnn_3/learning.py
+++ 2020/07/20200726_tensorflow_cnn_3/learning.py
@@ -1,10 +1,11 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 # noinspection PyShadowingNames
-def build(input_shape, classes):
+def build_model(input_shape, classes):
     model = models.Sequential()
 
     model.add(layers.Convolution2D(32, (3, 3), activation='relu', padding='same', input_shape=input_shape))
@@ -39,18 +40,28 @@ if __name__ == '__main__':
     IMG_ROWS = 32
     IMG_COLS = 32
 
-    BATCH_SIZE = 64
-    EPOCHS = 50
+    EPOCHS = 20
     CLASSES = 10
     VERBOSE = 1
     VALIDATION_SPLIT = 0.2
     OPTIM = tf.keras.optimizers.RMSprop()
 
     (train_data, train_labels), (test_data, test_labels) = datasets.cifar10.load_data()
+    train_data = train_data.astype('float32')
+    test_data = test_data.astype('float32')
+
     mean = np.mean(train_data, axis=(0, 1, 2, 3))
     std = np.std(train_data, axis=(0, 1, 2, 3))
     train_data = (train_data - mean) / (std + 1e-7)
     test_data = (test_data - mean) / (std + 1e-7)
+
+    datagen = ImageDataGenerator(
+        rotation_range=30,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True
+    )
+    datagen.fit(train_data)
 
     train_labels = tf.keras.utils.to_categorical(train_labels, CLASSES)
     test_labels = tf.keras.utils.to_categorical(test_labels, CLASSES)
@@ -59,12 +70,17 @@ if __name__ == '__main__':
         tf.keras.callbacks.TensorBoard(log_dir='./logs')
     ]
 
-    model = build((IMG_ROWS, IMG_ROWS, IMG_CHANNELS), CLASSES)
+    model = build_model(train_data.shape[1:], CLASSES)
     model.compile(loss='categorical_crossentropy', optimizer=OPTIM, metrics=['accuracy'])
-    model.fit(train_data, train_labels, batch_size=BATCH_SIZE,
-              epochs=EPOCHS, validation_split=VALIDATION_SPLIT,
-              verbose=VERBOSE, callbacks=callbacks)
+    model.fit_generator(datagen.flow(train_data, train_labels, batch_size=64),
+                        epochs=EPOCHS, verbose=VERBOSE, callbacks=callbacks,
+                        validation_data=(test_data, test_labels))
 
-    score = model.evaluate(test_data, test_labels, batch_size=BATCH_SIZE, verbose=VERBOSE)
+    model_json = model.to_json()
+    with open('model.json', 'w') as json_file:
+        json_file.write(model_json)
+    model.save_weights('model.h5')
+
+    score = model.evaluate(test_data, test_labels, batch_size=128, verbose=VERBOSE)
 
     print(f'Test score {score[0]}\nTest accuracy {score[1]}')
