892d35b1695204b5d1aae96966dabd2160ebcc18
Teddy Lee <teddy12090@gmail.com>
2020-08-02 22:57:17 +0800

    2020/08/02 Tensorflow inception v3 -2

---
+++ 2020/08/20200802_tensorflow_Inception_v3_2/learning.py
@@ -0,0 +1,16 @@
+from tensorflow.keras import layers, models
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
+if __name__ == '__main__':
+    base_model = InceptionV3(weights='imagenet', include_top=False)
+
+    x = base_model.output
+    x = layers.Dense(1024, activation='relu')(x)
+    predictions = layers.Dense(200, activation='softmax')(x)
+    model = models.Model(inputs=base_model.input, outputs=predictions)
+
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
