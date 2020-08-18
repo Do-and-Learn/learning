30ecaae3da5fc58b85019f4f0fd716c7bb77cbca
Teddy Lee <teddy12090@gmail.com>
2020-08-19 00:05:25 +0800

    2020/08/18 Deep convolutional generative adversarial network 2

---
+++ 2020/08/20200818_deep_convolutional_generative_adversarial_network_2/main.py
@@ -0,0 +1,131 @@
+import os
+
+import numpy as np
+import matplotlib.pylab as plt
+from tensorflow.keras import Input
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Dense, Reshape, UpSampling2D, Conv2D, BatchNormalization, Activation, LeakyReLU, Dropout, ZeroPadding2D, Flatten
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam
+
+
+class DCGAN:
+
+    def __init__(self, rows, cols, channels, z=100):
+        self.img_rows = rows
+        self.img_cols = cols
+        self.channels = channels
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        self.latent_dim = z
+
+        optimizer = Adam(0.0002, 0.5)
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+        self.generator = self.build_generator()
+        z = Input(shape=(self.latent_dim,))
+        img = self.generator(z)
+
+        valid = self.discriminator(img)
+
+        self.combined = Model(z, valid)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+    def build_generator(self):
+        model = Sequential()
+        model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))
+        model.add(Reshape((7, 7, 128)))
+        model.add(UpSampling2D())
+        model.add(Conv2D(128, kernel_size=3, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Activation("relu"))
+        model.add(UpSampling2D())
+        model.add(Conv2D(64, kernel_size=3, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Activation("relu"))
+        model.add(Conv2D(self.channels, kernel_size=3, padding="same"))
+        model.add(Activation("tanh"))
+        model.summary()
+        noise = Input(shape=(self.latent_dim,))
+        img = model(noise)
+        return Model(noise, img)
+
+    def build_discriminator(self):
+        model = Sequential()
+
+        model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+        model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+        img = Input(shape=self.img_shape)
+        validity = model(img)
+        return Model(img, validity)
+
+    def train(self, epochs, batch_size=128, save_interval=50):
+        (X_train, _), (_, _) = mnist.load_data()
+
+        # Rescale -1 to 1
+        X_train = X_train / 127.5 - 1.
+        X_train = np.expand_dims(X_train, axis=3)
+
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+
+        for epoch in range(epochs):
+            print(f'epoch: {epoch+1}')
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            imgs = X_train[idx]
+
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            gen_imgs = self.generator.predict(noise)
+
+            self.discriminator.trainable = True
+            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            self.discriminator.trainable = False
+            g_loss = self.combined.train_on_batch(noise, valid)
+
+            if epoch % save_interval == 0:
+                self.save_imgs(epoch)
+        self.save_imgs(epochs)
+
+    def save_imgs(self, epoch):
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, self.latent_dim))
+        gen_imgs = self.generator.predict(noise)
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        os.makedirs('images', exist_ok=True)
+        for i in range(r):
+            for j in range(c):
+                axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
+                axs[i, j].axis('off')
+                cnt += 1
+        fig.savefig(f'images/dcgan_mnist_{epoch:04}.png')
+        plt.close()
+
+
+if __name__ == '__main__':
+    dcgan = DCGAN(28, 28, 1)
+    dcgan.train(epochs=10000, batch_size=32, save_interval=500)
