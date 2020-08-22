64b46c091170d57f084a020b6970a7979a856c64
Teddy Lee <teddy12090@gmail.com>
2020-08-23 01:36:05 +0800

    add a reference link

--- 2020/08/20200822_download_cycle_gan_zip/main.py
+++ 2020/08/20200822_download_cycle_gan_zip/main.py
@@ -1,7 +1,7 @@
-import tensorflow_datasets as tfds
 import tensorflow as tf
 
 if __name__ == '__main__':
     # https://www.tensorflow.org/datasets/catalog/cycle_gan#cycle_gansummer2winter_yosemite
+    # https://www.tensorflow.org/tutorials/load_data/images
     data_dir = tf.keras.utils.get_file(origin='https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/summer2winter_yosemite.zip', fname='summer2winter_yosemite.zip')
     print(data_dir)
