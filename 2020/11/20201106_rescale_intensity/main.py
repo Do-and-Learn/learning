0636eed89442837efba9845e47074a0b89275b6b
Teddy Lee <teddy12090@gmail.com>
2020-11-02 00:30:21 +0800

    2020/11/06 rescale_intensity

---
+++ 2020/11/20201106_rescale_intensity/main.py
@@ -0,0 +1,16 @@
+import cv2
+from skimage import exposure
+from skimage.feature import hog
+
+
+def show_image(title, image, resize_rate=0.5):
+    image = cv2.resize(image, dsize=(int(img.shape[1] * resize_rate), int(img.shape[0] * resize_rate)))
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    img = cv2.imread('../../10/20201026_show_one_image/gaming.png')
+    obj, hog_image = hog(img, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True, multichannel=True)
+    hog_image = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+    show_image('hog_image', hog_image)
